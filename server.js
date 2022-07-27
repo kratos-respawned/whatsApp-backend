54e8f497874bbe3b88538deb8af847fbf7dc8e0b
Gaurav Bhandari <sbhandari1907@protonmail.com>
2022-07-28 03:34:33 +0530

    initial setup

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+const port = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
