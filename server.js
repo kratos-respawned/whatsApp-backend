198e5d0ec4fb7433be75ff52f66d4ce25c9fd35f
Gaurav Bhandari <sbhandari1907@protonmail.com>
2022-07-28 13:15:14 +0530

    pusher

--- server.js
+++ server.js
@@ -2,7 +2,16 @@ import express from "express";
 import mongoose from "mongoose";
 const app = express();
 import Messages from "./dbMessage.js";
-const port = process.env.PORT || 5000;
+import Pusher from "pusher";
+const port = process.env.PORT || 5274;
+const pusher = new Pusher({
+  appId: "1444124",
+  key: "a25c95cbd5b71ace78dd",
+  secret: "18293665311df6ff240e",
+  cluster: "ap2",
+  useTLS: true,
+});
+
 //db
 const url =
   "mongodb+srv://kratos1710:TdHT8KKCKXOLXueB@cluster0.cuzbp.mongodb.net/?retryWrites=true&w=majority";
@@ -10,12 +19,25 @@ mongoose.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+const db = mongoose.connection;
+db.once("open", () => {
+  console.log("db connected");
+  const msgCollection = db.collection("messagecontents");
+  const changeStream = msgCollection.watch();
+  changeStream.on("change", (change) => {
+    console.log(change);
+  });
+});
+
+//
 //routes
 app.get("/", (req, res) => {
   res.status(200).send("Hello World");
 });
 //middleware
 app.use(express.json());
+
 app.post("/messages/new", (req, res) => {
   const dbMessage = req.body;
 
@@ -24,6 +46,16 @@ app.post("/messages/new", (req, res) => {
       res.status(500).send(err);
     } else {
       res.status(201).send(data);
+    }
+  });
+});
+
+app.get("/messages/sync", (req, res) => {
+  Messages.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
     }
   });
 });
